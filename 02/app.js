1e2e4c01d9c152416bab33923f6534f2cb87c0b3
Wiktoria Spicha <135509462+spiwik98@users.noreply.github.com>
2024-06-05 20:41:08 +0200

    answer02-2

--- 02/app.js
+++ 02/app.js
@@ -5,3 +5,13 @@ for(let i = 0; i <= x; i++) {
     console.log(i);
     console.log('result:', result);
 }
+
+
+const x = 5;
+let result = 0;
+while (i <=x){
+    result = i + i;
+    console.log(i);
+    console.log('result:', result);
+    i++;
+}
