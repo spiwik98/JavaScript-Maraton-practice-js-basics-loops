fc1df305e36a13f708f89a9eb6490c4bc6c5a2d6
Wiktoria Spicha <135509462+spiwik98@users.noreply.github.com>
2024-06-05 20:54:06 +0200

    answer03

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,13 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+let randomNumber = Math.round(Math.random() * 11); 
+
+console.log(randomNumber);
+while (randomNumber !== x) {
+    iteration = iteration + 1;
+    randomNumber = Math.round(Math.random() * 11); 
+    console.log(`Iteration: ${iteration}, Random Number: ${randomNumber}`);
+}
+
+console.log(`It took ${iteration} iterations to generate the number ${x}.`);
