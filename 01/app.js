af1b91af9ce7c5c97133679425e8a49475aa6389
Wiktoria Spicha <135509462+spiwik98@users.noreply.github.com>
2024-06-05 20:20:51 +0200

    answer01

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,9 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
+if (randomNumber < 5){
+    console.log("Wylosowana liczba jest zbyt mała, aby użyć pętli")
+}else{
+for (let i = 5; i <= randomNumber; i++){
+console.log(i);
+}
+}
